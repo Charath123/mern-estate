ee5202314b6fdddba297a2f376e2f05eb16c37cc
Charath123 <charathkumar098@gmail.com>
2024-08-22 22:24:30 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="text-red-500">
+      <h1>Hello</h1>
+      <p>hi</p>
+    </div>
+  );
+};
+
+export default App;
