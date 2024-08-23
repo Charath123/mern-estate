32b45ce9d1677c9120e656ee0dda1dafca5f6800
Charath123 <charathkumar098@gmail.com>
2024-08-23 22:00:21 +0530

    api calls added

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.routes.js";
 dotenv.config();
 
 mongoose
@@ -17,3 +18,12 @@ const app = express();
 app.listen(3000, () => {
   console.log("server is running on 3000");
 });
+
+// app.get("/", (req, res) => {
+//   res.send("Hello world");
+// });
+
+// app.get("/test", (req, res) => {
+//   res.send("Hello Test");
+// });
+app.use("/api/user", userRouter);
