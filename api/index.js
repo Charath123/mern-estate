8be958253c458088a112321984eb49f94de062a3
Charath123 <charathkumar098@gmail.com>
2024-08-23 22:57:56 +0530

    Adding error handlers and signup users

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import userRouter from "./routes/user.routes.js";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 dotenv.config();
 
 mongoose
@@ -15,6 +16,8 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log("server is running on 3000");
 });
@@ -27,3 +30,16 @@ app.listen(3000, () => {
 //   res.send("Hello Test");
 // });
 app.use("/api/user", userRouter);
+
+app.use("/api/auth", authRouter);
+
+// this is middleware
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal server error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
