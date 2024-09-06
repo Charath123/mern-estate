b19c3f5c226b8236428e8baade5dc41ce092c2cd
Charath123 <charathkumar098@gmail.com>
2024-09-06 15:02:53 +0530

    Uploading images done

--- api/index.js
+++ api/index.js
@@ -3,40 +3,44 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
+import listingRouter from "./routes/listening.route.js";
+import path from "path";
+
 dotenv.config();
 
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
-    console.log("mongodb connected");
+    console.log("Connected to MongoDB");
   })
   .catch((err) => {
-    console.log(err);
+    console.log("Error: " + err);
   });
 
-const app = express();
+const __dirname = path.resolve();
 
+const app = express();
 app.use(express.json());
+app.use(cookieParser());
 
 app.listen(3000, () => {
-  console.log("server is running on 3000");
+  console.log("Server is running on port 3000");
 });
 
-// app.get("/", (req, res) => {
-//   res.send("Hello world");
-// });
-
-// app.get("/test", (req, res) => {
-//   res.send("Hello Test");
-// });
 app.use("/api/user", userRouter);
-
 app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
 
-// this is middleware
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
+
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const message = err.message || "Internal server error";
+  const message = err.message || "Internal Server Error";
   return res.status(statusCode).json({
     success: false,
     statusCode,
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,30 @@
 import React from "react";
 import { FaSearch } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
 
 const Header = () => {
+  const { currentUser } = useSelector((state) => state.user);
+  const [searchTerm, setSearchTerm] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set("searchTerm", searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get("searchTerm");
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
+
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -12,11 +34,16 @@ const Header = () => {
             <span className="text-slate-700">Estate</span>
           </h1>
         </Link>
-        <form className="flex bg-slate-100 p-3 rounded-lg items-center">
+        <form
+          onSubmit={handleSubmit}
+          className="flex bg-slate-100 p-3 rounded-lg items-center"
+        >
           <input
             type="text"
             placeholder="search..."
             className="bg-transparent focus:outline-none w-24 sm:w-64"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
           />
           <FaSearch className="text-slate-600" />
         </form>
@@ -31,10 +58,21 @@ const Header = () => {
               About
             </li>
           </Link>
-          <Link to="sign-in">
+          {/* <Link to="sign-in">
             <li className="sm:inline text-slate-700 hover:underline cursor-pointer">
               Sign In
             </li>
+          </Link> */}
+          <Link to="/profile">
+            {currentUser ? (
+              <img
+                src={currentUser.avatar}
+                alt="avatar"
+                className="rounded-full h-7 w-7 object-cover"
+              />
+            ) : (
+              <li className="text-slate-700 hover:underline">Sign In</li>
+            )}
           </Link>
         </ul>
       </div>
